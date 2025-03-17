0c4d29210026d36c9922da7f3fa6f9dcc00da498
ElioErrico <143315119+ElioErrico@users.noreply.github.com>
2025-03-17 22:40:34 +0100

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+const { can } = usePerms()
+
+const showSettings = computed(
+	() =>
+		can('READ', 'LLM') ||
+		can('READ', 'USERS') ||
+		can('READ', 'EMBEDDER') ||
+		can('READ', 'STATUS') ||
+		can('LIST', 'LLM') ||
+		can('LIST', 'USERS') ||
+		can('LIST', 'EMBEDDER') ||
+		can('LIST', 'STATUS'),
+)
+</script>
+
+<template>
+	<div class="navbar sticky top-0 z-30 min-h-fit bg-base-100 font-medium shadow-md md:px-[5%] lg:px-[10%]">
+		<div class="navbar-start">
+			<RouterLink to="/" class="shrink-0 !bg-transparent">
+				<img src="@assets/logo.svg" class="hidden size-12 cursor-pointer dark:brightness-0 dark:invert md:block" />
+			</RouterLink>
+			<Menu v-slot="{ open }" as="div" class="relative inline-block rounded-md md:hidden">
+				<MenuButton class="btn btn-circle btn-ghost" title="Menu">
+					<heroicons-x-mark-20-solid v-if="open" class="swap-on size-6" />
+					<heroicons-bars-3-solid v-else class="swap-off size-6" />
+				</MenuButton>
+				<Transition
+					enterActiveClass="transition duration-200 ease-out"
+					enterFromClass="transform scale-90 opacity-0"
+					enterToClass="transform scale-100 opacity-100"
+					leaveActiveClass="transition duration-200 ease-in"
+					leaveFromClass="transform scale-100 opacity-100"
+					leaveToClass="transform scale-90 opacity-0">
+					<MenuItems
+						as="ul"
+						class="menu menu-md absolute left-0 z-50 mt-4 w-min origin-top-left gap-2 whitespace-nowrap rounded-md bg-base-100 shadow-xl">
+						<MenuItem v-if="can('READ', 'CONVERSATION') || can('LIST', 'CONVERSATION')" as="li">
+							<RouterLink to="/"> <heroicons-home-20-solid class="size-4" /> Home </RouterLink>
+						</MenuItem>
+						<MenuItem v-if="can('READ', 'MEMORY') || can('LIST', 'MEMORY')" as="li">
+							<RouterLink :key="$route.fullPath" :to="{ path: '/memory' }"> <ph-brain-fill class="size-4" /> Memory </RouterLink>
+						</MenuItem>
+						<MenuItem v-if="can('READ', 'PLUGINS') || can('LIST', 'PLUGINS')" as="li">
+							<RouterLink :key="$route.fullPath" :to="{ path: '/plugins' }"> <ph-plug-fill class="size-4" /> Plugins </RouterLink>
+						</MenuItem>
+						<MenuItem v-if="showSettings" as="li">
+							<RouterLink :key="$route.fullPath" :to="{ path: '/settings' }" :class="{ active: $route.path === '/settings' }">
+								<heroicons-cog-6-tooth-20-solid class="size-4" /> Settings
+							</RouterLink>
+						</MenuItem>
+						<MenuItem as="li">
+							<a href="https://cheshire-cat-ai.github.io/docs/" target="_blank"> <heroicons-document-text-solid class="size-4" /> Docs </a>
+						</MenuItem>
+					</MenuItems>
+				</Transition>
+			</Menu>
+		</div>
+		<div class="navbar-center">
+			<RouterLink to="/" class="shrink-0 !bg-transparent">
+				<img src="@assets/logo.svg" class="size-12 cursor-pointer dark:brightness-0 dark:invert md:hidden" />
+			</RouterLink>
+			<ul class="menu menu-horizontal menu-md hidden gap-4 p-0 md:flex">
+				<li v-if="can('READ', 'CONVERSATION') || can('LIST', 'CONVERSATION')">
+					<RouterLink to="/"> <heroicons-home-20-solid class="size-4" /> Home </RouterLink>
+				</li>
+				<li v-if="can('READ', 'MEMORY') || can('LIST', 'MEMORY')">
+					<RouterLink :key="$route.fullPath" :to="{ path: '/memory' }"> <ph-brain-fill class="size-4" /> Memory </RouterLink>
+				</li>
+				<li v-if="can('READ', 'PLUGINS') || can('LIST', 'PLUGINS')">
+					<RouterLink :key="$route.fullPath" :to="{ path: '/plugins' }"> <ph-plug-fill class="size-4" /> Plugins </RouterLink>
+				</li>
+				<li v-if="showSettings">
+					<RouterLink :key="$route.fullPath" :to="{ path: '/settings' }" :class="{ active: $route.path === '/settings' }">
+						<heroicons-cog-6-tooth-20-solid class="size-4" /> Settings
+					</RouterLink>
+				</li>
+				<li>
+					<a href="https://cheshire-cat-ai.github.io/docs/" target="_blank"> <heroicons-document-text-solid class="size-4" /> Docs </a>
+				</li>
+			</ul>
+		</div>
+		<div class="navbar-end">
+			<UserDropdown />
+		</div>
+	</div>
+</template>
---
+++ vite.config.mts
@@ -0,0 +1,95 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { configDefaults, defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import { HeadlessUiResolver, VueUseComponentsResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import Unfonts from 'unplugin-fonts/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+
+export default defineConfig({
+	plugins: [
+		vue(),
+		AutoImport({
+			dts: true,
+			imports: ['vue', 'vue-router', '@vueuse/core', 'pinia', 'vitest'],
+			eslintrc: {
+				enabled: true,
+			},
+			dirs: ['./src/composables', './src/utils'],
+		}),
+		Components({
+			dts: true,
+			resolvers: [HeadlessUiResolver({ prefix: '' }), IconsResolver({ prefix: '' }), VueUseComponentsResolver()],
+		}),
+		Icons({ autoInstall: true }),
+		Unfonts({
+			custom: {
+				families: [
+					{
+						name: 'Rubik',
+						local: 'Rubik',
+						src: './src/assets/fonts/*.ttf',
+					},
+				],
+				display: 'auto',
+				preload: true,
+				prefetch: false,
+			},
+		}),
+		tsconfigPaths(),
+		{
+			name: 'configure-token',
+			configureServer(server) {
+				return () => {
+					server.middlewares.use(async (_, res, next) => {
+						const output = await (
+							await fetch('http://localhost:1865/auth/token', {
+								method: 'POST',
+								headers: {
+									'Content-Type': 'application/json',
+								},
+								body: JSON.stringify({
+									username: 'admin',
+									password: 'admin',
+								}),
+							})
+						).json()
+						res.setHeader('Set-Cookie', `ccat_user_token=${output.access_token}`)
+						next()
+					})
+				}
+			},
+		},
+	],
+	test: {
+		environment: 'jsdom',
+		globals: true,
+		exclude: [...configDefaults.exclude, 'e2e/*'],
+	},
+	server: {
+		port: 3000,
+		open: false,
+		host: true,
+	},
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		cssCodeSplit: false,
+		rollupOptions: {
+			output: {
+				minifyInternalExports: true,
+				entryFileNames: 'assets/cat.js',
+				assetFileNames: info => `assets/${info.name?.endsWith('css') ? 'cat' : '[name]'}[extname]`,
+				chunkFileNames: 'chunk.js',
+				manualChunks: () => 'chunk.js',
+				generatedCode: {
+					preset: 'es2015',
+					constBindings: true,
+					objectShorthand: true,
+				},
+			},
+		},
+	},
+})
